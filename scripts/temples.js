203b18257879fb3fdbf982ba5507792c5cf9516b
brandsharky <brandsharky@gmail.com>
2026-01-16 10:40:28 -0800

    Fix header and nav so that it actually works like shown and asked for

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,17 +1,14 @@
-
 // Current Year and Last Modified
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML += document.lastModified;
 
-
-
 // Toggle Hamburger Menu
-const menuButton = document.getElementById("menu-button");
-const navMenu = document.getElementById("nav-menu");
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
 
-menuButton.addEventListener("click", () => {
-  navMenu.classList.toggle("show");
-  menuButton.textContent = navMenu.classList.contains("show") ? "✖" : "☰";
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamburger.textContent = navLinks.classList.contains("active") ? "✖" : "☰";
 });
 
 
